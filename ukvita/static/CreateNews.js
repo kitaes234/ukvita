c7b15992721daf4d23fdc58857b26181bbca5b6b
kitaes234 <maksimenko20021@gmail.com>
2022-04-02 04:35:52 +0300

    'v0.1.02'

--- ukvita/static/CreateNews.js
+++ ukvita/static/CreateNews.js
@@ -245,8 +245,8 @@ function WindowCreateStatistics() {
       success: function(data) {
         data.forEach(function (curr_obj) {
             i++
-            console.log(curr_obj)
-            $('#DashboardRows').empty();
+            console.log(curr_obj.length)
+$('#DashboardRows').empty();
             $('#DashboardRows').append(
                 '<div class="alert alert-warning" id="HelpNumber'+ curr_obj.id +'">' +
                 '<input type="hidden" name="csrfmiddlewaretoken" value="'+ obj.csrf_token +'">' +
